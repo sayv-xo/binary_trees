155d94f1200cd73d25236bdef188db92631ec1ed
sayv <98850746+sayv-xo@users.noreply.github.com>
2023-11-02 07:39:13 +0000

    modified function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new = binary_tree_node(parent, value);
 
-	if (new == NULL || parent == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
