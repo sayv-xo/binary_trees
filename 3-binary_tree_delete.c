3b211c20077523eb6f87476a0ccb4aa7da95df38
sayv <98850746+sayv-xo@users.noreply.github.com>
2023-11-03 00:29:02 +0000

    fixed betty error for 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_delete - delete a binary tree
- * @tree - pointer to root node of tree to delete
+ * @tree: pointer to root node of tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
